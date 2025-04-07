794040f26dbdcc006a8bed5c4664733ef5954464
Morozov Vladyslav <vladmorozov@knu.ua>
2025-04-07 14:38:38 +0300

    created basic endpoint working with db

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -25,6 +25,7 @@ func SetupRoutes() *gin.Engine {
 	router.Use(cors.New(config))
 
 	router.GET("/", indexView)
+	router.GET("/todo/:item", CreateTodoItem)
 
 	// Додайте ваші маршрути для роботи з БД тут
 
@@ -65,4 +66,39 @@ func main() {
 	router.Run(":8081")
 }
 
+type ListItem struct {
+	Id   string `json:"id"`
+	Item string `json:"item"`
+	Done bool   `json:"done"`
+}
 
+func CreateTodoItem(c *gin.Context) {
+	item := c.Param("item")
+
+	// Validate item
+	if len(item) == 0 {
+		c.JSON(http.StatusNotAcceptable, gin.H{"message": "please enter an item"})
+		return
+	}
+
+	// Create todo item struct
+	var TodoItem ListItem
+	TodoItem.Item = item
+	TodoItem.Done = false
+
+	// Insert item into DB and return the generated ID
+	err := db.QueryRow("INSERT INTO list(item, done) VALUES($1, $2) RETURNING id;", TodoItem.Item, TodoItem.Done).Scan(&TodoItem.Id)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "error with DB"})
+		return
+	}
+
+	// Log message
+	log.Println("created todo item with ID", TodoItem.Id)
+
+	// Return success response with item ID
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+	c.JSON(http.StatusCreated, gin.H{"item": TodoItem})
+}
